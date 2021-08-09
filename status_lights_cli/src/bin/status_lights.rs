156544dd80fada2b73dbf96fb622a727b0f903fc
Daniel Mason <daniel@danielmason.com>
2021-08-09 23:07:08 +0100

    use struct opt to begin implementing interface

--- status_lights_cli/src/bin/status_lights.rs
+++ status_lights_cli/src/bin/status_lights.rs
@@ -1,8 +1,63 @@
 use status_lights_cli::get_status_light_devices;
 
+use structopt::StructOpt;
+use rusb::{GlobalContext, Device};
+
+#[derive(Debug, StructOpt)]
+struct BackgroundOptions {
+    led: u8,
+    red: u8,
+    green: u8,
+    blue: u8,
+    device: Option<u8>,
+}
+
+#[derive(Debug, StructOpt)]
+struct ForegroundOptions {
+    led: u8,
+    red: u8,
+    green: u8,
+    blue: u8,
+    seconds: Option<u8>,
+    device: Option<u8>,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "status_lights", about = "Control status lights")]
+enum Opt {
+    List,
+    Background(BackgroundOptions),
+    Foreground(ForegroundOptions),
+}
+
 fn main() {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+
     let devices = get_status_light_devices();
-    for device in devices {
-        println!("{:?}", device);
+
+    match opt {
+        Opt::List => print_devices_addresses(devices),
+        _ => {}
     }
 }
+
+fn print_devices_addresses(devices: Vec<Device<GlobalContext>>) {
+    // Get just the addresses and sort them
+    let mut addresses = devices.iter()
+        .map(|d| d.address())
+        .collect::<Vec<u8>>();
+    addresses.sort();
+
+    // Print the output
+    print!("Found {} devices", addresses.len());
+    if let Some((last, rest)) = addresses.split_last() {
+        print!(" at addresses: ");
+        for address in rest {
+            print!("{}, ", address);
+        }
+        print!("{}", last);
+    }
+    println!();
+}
+
